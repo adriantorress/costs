155b669b8688c694df8cbc2098cf67dfad33f56d
adriantorress <adriantorrescv@gmail.com>
2023-01-20 03:25:16 -0300

    Projeto Costs

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,15 @@
+import { FaInstagram, FaLinkedin, FaGithub } from 'react-icons/fa'
+import styles from './Footer.module.css'
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <ul className={styles.social_list}>
+        <li><FaInstagram /></li>
+        <li><FaLinkedin /></li>
+        <li><FaGithub /></li>
+      </ul>
+      <p className={styles.copy_right}><span>Costs</span> &copy; 2023</p>
+    </footer>
+  )
+};
